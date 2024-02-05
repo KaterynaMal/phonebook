a5352abcac609ee060da842315dcd59431ab81a1
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-02-05 13:52:02 +0000

    Deploying to gh-pages from @ KaterynaMal/goit-react-hw-08-phonebook@11646e023b6c5f1e47e1701a5b15cdcb16b3c378 🚀

---
+++ static/js/75.49d84389.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{1075:function(t,e,n){n.r(e),n.d(e,{default:function(){return s}});n(2791);var o=n(7689),c="Contacts_form_container__EhZdb",a=n(3942),r=n(184),s=function(){var t=(0,o.TH)();return(0,r.jsxs)("div",{className:c,children:[(0,r.jsx)("h1",{children:"Phonebook"}),(0,r.jsx)(a.t,{location:t}),(0,r.jsx)("h2",{children:"Contacts"}),(0,r.jsx)(a.wn,{location:t}),(0,r.jsx)(a.CR,{location:t})]})}}}]);
+//# sourceMappingURL=75.49d84389.chunk.js.map
